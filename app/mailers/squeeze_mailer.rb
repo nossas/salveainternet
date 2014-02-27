05801fb5aa2a7467432144a19d02b62b834a080f
Nícolas Iensen <nicolas@engage.is>
2014-02-27 19:51:48 -0300

    Add SqueezeMailer views

--- app/mailers/squeeze_mailer.rb
+++ app/mailers/squeeze_mailer.rb
@@ -1,3 +1,11 @@
 class SqueezeMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "Alessandra - Meu Rio <contato@meurio.org.br>"
+
+  def dont_let_them_limit_our_access_to_the_internet squeeze
+    mail(to: squeeze.email, subject: "Não deixe o deputado Eduardo Cunha limitar nosso acesso à internet")
+  end
+
+  def you_cant_negotiate_the_neutrality_of_the_internet squeeze
+    mail(to: DEPUTIES_EMAILS, subject: "Deputado, a neutralidade da rede é inegociável")
+  end
 end
---
+++ config/initializers/deputies.rb
@@ -0,0 +1,29 @@
+if Rails.env.production?
+  DEPUTIES_EMAILS = [
+    "Vicentinho <dep.vicentinho@camara.leg.br>",
+    "Eduardo Cunha <dep.eduardocunha@camara.leg.br>",
+    "Eduardo da Fonte <dep.eduardodafonte@camara.leg.br>",
+    "Antonio Imbassahy <dep.antonioimbassahy@camara.leg.br>",
+    "Moreira Mendes <dep.moreiramendes@camara.leg.br>",
+    "Anthony Garotinho <dep.anthonygarotinho@camara.leg.br>",
+    "Mendonça Filho <dep.mendoncafilho@camara.leg.br>",
+    "Beto Albuquerque <dep.betoalbuquerque@camara.leg.br>",
+    "Fernando Francischini <dep.fernandofrancischini@camara.leg.br>",
+    "Vieira da Cunha <dep.vieiradacunha@camara.leg.br>",
+    "Jovair Arantes <dep.jovairarantes@camara.leg.br>",
+    "Jandira Feghali <dep.jandirafeghali@camara.leg.br>",
+    "André Moura <dep.andremoura@camara.leg.br>",
+    "George Hilton <dep.georgehilton@camara.leg.br>",
+    "Sarney Filho <dep.sarneyfilho@camara.leg.br>",
+    "Rubens Bueno <dep.rubensbueno@camara.leg.br>",
+    "Ivan Valente <dep.ivanvalente@camara.leg.br>",
+    "Francisco Tenório <dep.franciscotenorio@camara.leg.br>",
+    "Arlindo Chinaglia <dep.arlindochinaglia@camara.leg.br>",
+    "Domingos Sávio <dep.domingossavio@camara.leg.br>",
+    "Givaldo Carimbão <dep.givaldocarimbao@camara.leg.br>",
+    "Luis Tibé <dep.luistibe@camara.leg.br>",
+    "Chico das Verduras <dep.chicodasverduras@camara.leg.br>"
+  ]
+else
+  DEPUTIES_EMAILS = ENV["DEPUTIES_EMAILS"]
+end
