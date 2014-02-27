80e3fe7fde9d729d0b971ad8ce7bcfabfb3c0b86
Nícolas Iensen <nicolas@engage.is>
2014-02-27 10:51:34 -0300

    Add subscription controller and model; Change root route to subscriptions#new

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Salveainternet::Application.routes.draw do
+  root 'subscriptions#new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
