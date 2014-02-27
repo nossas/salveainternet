b94f32ae7e1b3927b87748ed715b0a2729210460
Nícolas Iensen <nicolas@engage.is>
2014-02-27 10:01:43 -0300

    Rails template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Salveainternet::Application.config.secret_key_base = '4231cb311a363a9c7508a311894032b4e05930a1c31974244c47014908e4bd0328bd87bdf6ae9a15f5d5a06b9c587220b41ead06533f7ba3108cee5b83f38eb4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Salveainternet::Application.config.session_store :cookie_store, key: '_salveainternet_session'
