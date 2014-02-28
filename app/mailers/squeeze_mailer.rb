5d6f21d6b5e0c4c1b42434259993c6be99bff537
Nícolas Iensen <nicolas@engage.is>
2014-02-27 23:07:42 -0300

    Add header to emails

--- app/mailers/squeeze_mailer.rb
+++ app/mailers/squeeze_mailer.rb
@@ -2,10 +2,12 @@ class SqueezeMailer < ActionMailer::Base
   default from: "Alessandra - Meu Rio <contato@meurio.org.br>"
 
   def dont_let_them_limit_our_access_to_the_internet squeeze
+    headers "X-SMTPAPI" => "{ \"category\": [\"salveainternet\", \"dont_let_them_limit_our_access_to_the_internet\"] }"
     mail(to: squeeze.email, subject: "Não deixe o deputado Eduardo Cunha limitar nosso acesso à internet")
   end
 
   def you_cant_negotiate_the_neutrality_of_the_internet squeeze
+    headers "X-SMTPAPI" => "{ \"category\": [\"salveainternet\", \"you_cant_negotiate_the_neutrality_of_the_internet\"] }"
     mail(to: DEPUTIES_EMAILS, subject: "Deputado, a neutralidade da rede é inegociável", from: "#{squeeze.first_name} #{squeeze.last_name} <#{squeeze.email}>")
   end
 end
