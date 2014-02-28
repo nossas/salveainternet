f5efa5c9e7292c09f0def95ff4c5ba0103182be4
Nícolas Iensen <nicolas@engage.is>
2014-02-28 10:23:02 -0300

    Change copy of email

--- app/mailers/squeeze_mailer.rb
+++ app/mailers/squeeze_mailer.rb
@@ -2,8 +2,9 @@ class SqueezeMailer < ActionMailer::Base
   default from: "Alessandra - Meu Rio <contato@meurio.org.br>"
 
   def dont_let_them_limit_our_access_to_the_internet squeeze
+    @squeeze = squeeze
     headers "X-SMTPAPI" => "{ \"category\": [\"salveainternet\", \"dont_let_them_limit_our_access_to_the_internet\"] }"
-    mail(to: squeeze.email, subject: "Não deixe o deputado Eduardo Cunha limitar nosso acesso à internet")
+    mail(to: @squeeze.email, subject: "Não deixe o deputado Eduardo Cunha limitar nosso acesso à internet")
   end
 
   def you_cant_negotiate_the_neutrality_of_the_internet squeeze
