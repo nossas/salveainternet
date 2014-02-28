485cd3448e77b3da1c2643e8a8eb54a8d281bdd1
Nícolas Iensen <nicolas@engage.is>
2014-02-28 13:29:30 -0300

    Add parallax effects

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,7 @@
 //= require foundation
 //= require turbolinks
 //= require jquery.facebox
+//= require jquery.stellar
 //= require_tree .
 
 $.facebox.settings.closeImage = 'http://i.imgur.com/k9awT9O.png'
@@ -42,6 +43,8 @@ $(function() {
     return false;
   });
 
+  $.stellar();
+
   new_squeeze_y = $(".new_squeeze").position().top
   $(window).scroll(function(){
     if(window.scrollY >= new_squeeze_y){
@@ -54,11 +57,39 @@ $(function() {
     }
 
     // Parallax
-    // scrollProgress = ((window.scrollY/$(document).height()) * 100) + "%";
-    // invertedScrollProgress = (100 - ((window.scrollY/$(document).height()) * 100)) + "%";
-    // $(".slide-2").css("background-position", scrollProgress + " " + scrollProgress);
-    // $(".slide-3").css("background-position-y", scrollProgress);
-    // $(".slide-4").css("background-position-x", scrollProgress);
-    // $(".slide-5").css("background-position-x", invertedScrollProgress);
+    scrollRatio = window.scrollY/$(document).height();
+    scrollProgress = ((window.scrollY/$(document).height()) * 100) + "%";
+    invertedScrollProgress = (100 - ((window.scrollY/$(document).height()) * 100)) + "%";
+    scrollProgressPx = ((window.scrollY/$(document).height()) * 1000) + "px";
+    invertedScrollProgressPx = (10000 - (window.scrollY/$(document).height()) * 10000) + "px";
+    
+    slide3ScrollRatio = window.scrollY/$(".slide-3").position().top
+    slide3ScrollRatio = slide3ScrollRatio > 1 ? 1 : slide3ScrollRatio
+    slide3BgHeight = 720;
+    slide3Height = $(".slide-3").height() + parseInt($(".slide-3").css("padding-top")) + parseInt($(".slide-3").css("padding-bottom"));
+    slide3Offset = slide3BgHeight - slide3Height;
+    slide3Position = (-slide3Offset * slide3ScrollRatio) + "px";
+
+    slide6ScrollRatio = window.scrollY/$(".slide-6").position().top
+    slide6ScrollRatio = slide6ScrollRatio > 1 ? 1 : slide6ScrollRatio
+    slide6BgHeight = 720;
+    slide6Height = $(".slide-6").height() + parseInt($(".slide-6").css("padding-top")) + parseInt($(".slide-6").css("padding-bottom"));
+    slide6Offset = slide6BgHeight - slide6Height;
+    slide6Position = (-slide6Offset * slide6ScrollRatio) + "px";
+
+    slide7ScrollRatio = window.scrollY/$(".slide-7").position().top
+    slide7ScrollRatio = slide7ScrollRatio > 1 ? 1 : slide7ScrollRatio
+    slide7BgHeight = 720;
+    slide7Height = $(".slide-7").height() + parseInt($(".slide-7").css("padding-top")) + parseInt($(".slide-7").css("padding-bottom"));
+    slide7Offset = slide7BgHeight - slide7Height;
+    slide7Position = (-slide7Offset * slide7ScrollRatio) + "px";
+    
+    $(".slide-1").css("background-position", "center " + scrollProgressPx);
+    $(".slide-2").css("background-position-x", scrollProgress);
+    $(".slide-3").css("background-position", "center " + slide3Position);
+    $(".slide-4").css("background-position-x", scrollProgress);
+    $(".slide-5").css("background-position-x", invertedScrollProgress);
+    $(".slide-6").css("background-position", "center " + slide6Position);
+    $(".slide-7").css("background-position", "center " + slide7Position);
   });
 });
