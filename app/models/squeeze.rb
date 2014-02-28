5144bd1e55bf8a85c725cfffec8963e75efad314
Nícolas Iensen <nicolas@engage.is>
2014-02-27 21:36:39 -0300

    Add validations to squeeze

--- app/models/squeeze.rb
+++ app/models/squeeze.rb
@@ -1,4 +1,6 @@
 class Squeeze < ActiveRecord::Base
+  validates :first_name, :last_name, :email, presence: true
+
   after_create { SqueezeMailer.delay.dont_let_them_limit_our_access_to_the_internet(self) }
   after_create { SqueezeMailer.delay.you_cant_negotiate_the_neutrality_of_the_internet(self) }
 end
