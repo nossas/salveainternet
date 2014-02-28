153fc1f737649293dd71bd25a3908d5f90b56119
Nícolas Iensen <nicolas@engage.is>
2014-02-27 23:03:15 -0300

    Add uniqueness validation for Squeeze#email

--- spec/models/squeeze_spec.rb
+++ spec/models/squeeze_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe Squeeze do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { Fabricate(:squeeze) }
+
+  it { should validate_presence_of :first_name }
+  it { should validate_presence_of :last_name }
+  it { should validate_presence_of :email }
+  it { should validate_uniqueness_of :email }
 end
