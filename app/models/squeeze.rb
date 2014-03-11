77efae450e8bbb8cae611394e410a7ea7a71f3c8
Nícolas Iensen <nicolas@engage.is>
2014-03-11 16:45:19 -0300

    [#67263346] add Mailchimp integration

--- app/models/squeeze.rb
+++ app/models/squeeze.rb
@@ -4,4 +4,16 @@ class Squeeze < ActiveRecord::Base
 
   after_create { SqueezeMailer.delay.dont_let_them_limit_our_access_to_the_internet(self) }
   after_create { SqueezeMailer.delay.you_cant_negotiate_the_neutrality_of_the_internet(self) }
+  after_create { self.delay.add_to_mailchimp_segment }
+
+  def add_to_mailchimp_segment
+    if !Rails.env.test?
+      begin
+        Gibbon::API.lists.subscribe(id: ENV["MAILCHIMP_LIST_ID"], email: {email: self.email}, merge_vars: {FNAME: self.first_name, LNAME: self.last_name}, double_optin: false, update_existing: true)
+        Gibbon::API.lists.static_segment_members_add(id: ENV["MAILCHIMP_LIST_ID"], seg_id: 3297, batch: [{email: self.email}])
+      rescue Exception => e
+        Rails.logger.error e
+      end
+    end
+  end
 end
---
+++ config/initializers/03_check_env_vars.rb
@@ -0,0 +1,2 @@
+raise "MAILCHIMP_LIST_ID is missing" if ENV["MAILCHIMP_LIST_ID"].nil?
+raise "MAILCHIMP_API_KEY is missing" if ENV["MAILCHIMP_API_KEY"].nil?
