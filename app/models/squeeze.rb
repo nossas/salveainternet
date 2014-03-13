3df3531061c880a85b665fa0033af2f2e7450182
Nícolas Iensen <nicolas@engage.is>
2014-03-13 18:28:30 -0300

    [#67307670] add city into the mailchimp fields

--- app/models/squeeze.rb
+++ app/models/squeeze.rb
@@ -10,7 +10,7 @@ class Squeeze < ActiveRecord::Base
   def add_to_mailchimp_segment
     if !Rails.env.test?
       begin
-        Gibbon::API.lists.subscribe(id: ENV["MAILCHIMP_LIST_ID"], email: {email: self.email}, merge_vars: {FNAME: self.first_name, LNAME: self.last_name}, double_optin: false, update_existing: true)
+        Gibbon::API.lists.subscribe(id: ENV["MAILCHIMP_LIST_ID"], email: {email: self.email}, merge_vars: {FNAME: self.first_name, LNAME: self.last_name, CITY: self.city}, double_optin: false, update_existing: true)
         Gibbon::API.lists.static_segment_members_add(id: ENV["MAILCHIMP_LIST_ID"], seg_id: ENV["MAILCHIMP_SEGMENT_ID"], batch: [{email: self.email}])
       rescue Exception => e
         Rails.logger.error e
