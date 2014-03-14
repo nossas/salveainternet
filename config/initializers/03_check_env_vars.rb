2c4ea08b5ea4b4e4ce9c1af8f214673bc98863a4
Nícolas Iensen <nicolas@engage.is>
2014-03-14 10:53:10 -0300

    [#67549352] add omniauth initializer

--- config/initializers/03_check_env_vars.rb
+++ config/initializers/03_check_env_vars.rb
@@ -2,4 +2,6 @@ if Rails.env.production? || Rails.env.staging?
   raise "MAILCHIMP_LIST_ID is missing" if ENV["MAILCHIMP_LIST_ID"].nil?
   raise "MAILCHIMP_API_KEY is missing" if ENV["MAILCHIMP_API_KEY"].nil?
   raise "MAILCHIMP_SEGMENT_ID is missing" if ENV["MAILCHIMP_SEGMENT_ID"].nil?
+  raise "FACEBOOK_KEY is missing" if ENV["FACEBOOK_KEY"].nil?
+  raise "FACEBOOK_SECRET is missing" if ENV["FACEBOOK_SECRET"].nil?
 end
