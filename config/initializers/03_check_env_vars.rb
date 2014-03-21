55f7a6e57021137515d93a3dc3d811fcc49db935
Nícolas Iensen <nicolas@engage.is>
2014-03-21 14:13:24 -0300

    [#67960048] add twitter env vars to checker

--- config/initializers/03_check_env_vars.rb
+++ config/initializers/03_check_env_vars.rb
@@ -4,4 +4,6 @@ if Rails.env.production? || Rails.env.staging?
   raise "MAILCHIMP_SEGMENT_ID is missing" if ENV["MAILCHIMP_SEGMENT_ID"].nil?
   raise "FACEBOOK_KEY is missing" if ENV["FACEBOOK_KEY"].nil?
   raise "FACEBOOK_SECRET is missing" if ENV["FACEBOOK_SECRET"].nil?
+  raise "TWITTER_KEY is missing" if ENV["TWITTER_KEY"].nil?
+  raise "TWITTER_SECRET is missing" if ENV["TWITTER_SECRET"].nil?
 end
