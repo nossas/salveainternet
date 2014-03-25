f40aca3ce0192c772f348147ed3f3bc2f856c3f5
Josias Schneider <josias.ds@gmail.com>
2014-03-25 20:20:56 -0300

    Victory: change emails subject

--- app/mailers/squeeze_mailer.rb
+++ app/mailers/squeeze_mailer.rb
@@ -4,11 +4,11 @@ class SqueezeMailer < ActionMailer::Base
   def dont_let_them_limit_our_access_to_the_internet squeeze
     @squeeze = squeeze
     headers "X-SMTPAPI" => "{ \"category\": [\"salveainternet\", \"dont_let_them_limit_our_access_to_the_internet\"] }"
-    mail(to: @squeeze.email, subject: "Não deixe o deputado Eduardo Cunha limitar nosso acesso à internet")
+    mail(to: @squeeze.email, subject: "Pressão nos senadores: use a internet para salvar a internet!")
   end
 
   def you_cant_negotiate_the_neutrality_of_the_internet squeeze
     headers "X-SMTPAPI" => "{ \"category\": [\"salveainternet\", \"you_cant_negotiate_the_neutrality_of_the_internet\"] }"
-    mail(to: DEPUTIES_EMAILS, subject: "Deputado, mantenha o artigo 20 do Marco Civil!", from: "#{squeeze.first_name} #{squeeze.last_name} <#{squeeze.email}>")
+    mail(to: DEPUTIES_EMAILS, subject: "Senador, aprove o Marco Civil votado pela Câmara", from: "#{squeeze.first_name} #{squeeze.last_name} <#{squeeze.email}>")
   end
 end
