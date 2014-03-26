60b13a060111e6ed1de96bacea4ab3d3e3d12857
Josias Schneider <josias.ds@gmail.com>
2014-03-25 21:34:56 -0300

    change mail sending to squeezes_controller#create

--- app/controllers/squeezes_controller.rb
+++ app/controllers/squeezes_controller.rb
@@ -2,6 +2,13 @@ class SqueezesController < InheritedResources::Base
   before_filter only: [:create] { params[:squeeze][:ip] = request.remote_ip }
 
   def create
+    begin
+      SqueezeMailer.delay.dont_let_them_limit_our_access_to_the_internet(@squeeze)
+      SqueezeMailer.delay.you_cant_negotiate_the_neutrality_of_the_internet(@squeeze)
+    rescue Exception => e
+      Rails.logger.error e
+    end
+
     create! do |success, failure|
       success.html { redirect_to root_path(anchor: "share") }
       failure.html { redirect_to root_path(anchor: "share") }
