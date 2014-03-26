02d9bcb555939b9128451feea8e7cba3cdcbf716
Nícolas Iensen <nicolas@engage.is>
2014-03-25 22:11:52 -0300

    Change squeezes controller

--- app/controllers/squeezes_controller.rb
+++ app/controllers/squeezes_controller.rb
@@ -2,8 +2,10 @@ class SqueezesController < InheritedResources::Base
   before_filter only: [:create] { params[:squeeze][:ip] = request.remote_ip }
 
   def create
-    @squeeze.delay.send_mail
-    create! { redirect_to root_path(anchor: "share") }
+    create! do
+      resource.delay.send_mail
+      redirect_to root_path(anchor: "share")
+    end
   end
 
   def permitted_params
