40898e222395298521bffd8e6dc7c57abdfc1993
Nícolas Iensen <nicolas@engage.is>
2014-03-26 16:53:39 -0300

    Replace Sendgrid with Mandrill in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,12 +83,13 @@ Salveainternet::Application.configure do
   default_url_options[:host] = "salveainternet.meurio.org.br"
 
   ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => ENV['HOST'],
-    :enable_starttls_auto => true
+    :port =>           '587',
+    :address =>        'smtp.mandrillapp.com',
+    :user_name =>      ENV['MANDRILL_USERNAME'],
+    :password =>       ENV['MANDRILL_APIKEY'],
+    :domain =>         'salveainternet.meurio.org.br',
+    :authentication => :plain
   }
+
+  ActionMailer::Base.delivery_method = :smtp
 end
